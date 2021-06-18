cd3ce1147c2543b75dcd20946c3a98bf1388ff92
Ahmad Rulim <mfalkia342@gmail.com>
2021-06-18 21:39:16 +0700

    Fix CORS
    Signed-off-by: Ahmad Rulim <mfalkia342@gmail.com>

--- src/scripts/datasource/covid-datasource.js
+++ src/scripts/datasource/covid-datasource.js
@@ -2,7 +2,7 @@ import CONFIG from '../global/config';
 
 class CovidDataSource {
   static async getData() {
-    const response = await fetch(CONFIG.COVID_API_URL);
+    const response = await fetch(`https://cors.bridged.cc/${CONFIG.COVID_API_URL}`);
     const responseJson = await response.json();
     let data = {};
     responseJson.forEach((element) => {
